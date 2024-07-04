7de88b54adf0fffc6b3ba4782227fd5611b6e8b1
maashi <octtn8c@gmail.com>
2024-07-05 01:00:32 +0900

    add: 静的ページと共存テスト

--- src/components/Pages/Top/Top.tsx
+++ src/components/Pages/Top/Top.tsx
@@ -24,8 +24,24 @@ export const Top = (props: Props) => {
       </ul>
       <ul className="grid gap-x-4">
         <li>
-          <Link href="/about">About</Link>
+          <Link href="/news/test1/">静的ページテスト1</Link>
         </li>
+        <li>
+          <Link href="/news/test1/index.html">
+            静的ページテスト1：index.htmlあり
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/test2/">静的ページテスト2</Link>
+        </li>
+        <li>
+          <Link href="/news/test2/index.html">
+            静的ページテスト2：index.htmlあり
+          </Link>
+        </li>
+        {/* <li>
+          <Link href="/about">About</Link>
+        </li> */}
       </ul>
 
       <Button text="サンプルボタン" />
